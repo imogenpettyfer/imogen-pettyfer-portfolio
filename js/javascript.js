d362770da7ee90ab23eb274a2b22571125c40d3b
imogenpettyfer <imogenpettyfer@my.capilanou.ca>
2023-03-16 21:22:51 -0700

    Revert "updated"
    This reverts commit 3a7c9d81187e69d1f3948dd35e4176029c4f4eb4.

--- js/javascript.js
+++ js/javascript.js
@@ -9,7 +9,8 @@ const elts = {
 
 // The strings to morph between. You can change these to anything you want!
 const texts = [
-	"Imogen Pettyfer", "A Designer", "Organized", "Collaborative", "A Photographer", "Intuitive", "Observant",];
+	"Imogen Pettyfer", "A Designer", "Organized", "Collaborative", "A Photographer", "Intuitive", "Observant", "Visuals", ""
+];
 
 // Controls the speed of morphing.
 const morphTime = 1;
